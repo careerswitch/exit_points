f5d3a1b6557a4a7a321c1574c47692d5404578c9
Ahmet Bolat <abolat01@gmail.com>
2019-09-20 18:15:23 -0400

    Update exit.py

--- exit.py
+++ exit.py
@@ -3,11 +3,13 @@
 This script will calculate your potential exit points.
 '''
 
+#variables
 def main():
     position = input('Enter L for Long or S for Short = ').lower()
     shares = float(input('Enter position size = '))
     avg_cost = float(input('Enter average cost of shares = '))
     
+#calculating position
     if position == 'l':
         print('$25 = ' + str(round(25 / shares + avg_cost, 2)) + '\n') 
         print('$50 = ' + str(round(50 / shares + avg_cost, 2)) + '\n')
@@ -17,7 +19,7 @@ def main():
         print('$150 = ' + str(round(150 / shares + avg_cost, 2)) + '\n')
         print('$175 = ' + str(round(175 / shares + avg_cost, 2)) + '\n')
         print('$200 = ' + str(round(200 / shares + avg_cost, 2)) + '\n')
-    
+ 
     elif position == 's':
         print('$25 = ' + str(round(avg_cost - 25 / shares, 2)) + '\n')
         print('$50 = ' + str(round(avg_cost - 50 / shares, 2)) + '\n')
@@ -30,7 +32,7 @@ def main():
     
     else:
         print('Something went wrong!')
-#    print('See you soon!')
+
 
 
 if __name__ == '__main__':
